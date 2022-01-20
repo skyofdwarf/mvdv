270f333dd4bd5aba28d18f013df59983df8ccb82
yeongjung.kim <skyofdwarf@gmail.com>
2022-01-20 21:39:29 +0900

    Add basic view controllers: Main, Home, Upcoming, Search, Profile

---
+++ MVDV/ViewControllers/Tab/Upcoming/UpcomingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UpcomingViewController.swift
+//  MVDV
+//
+//  Created by YEONGJUNG KIM on 2022/01/21.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- R.generated.swift
+++ R.generated.swift
@@ -219,7 +219,7 @@ struct _R: Rswift.Validatable {
 
     #if os(iOS) || os(tvOS)
     struct main: Rswift.StoryboardResourceWithInitialControllerType, Rswift.Validatable {
-      typealias InitialController = ViewController
+      typealias InitialController = MainViewController
 
       let bundle = R.hostingBundle
       let name = "Main"
